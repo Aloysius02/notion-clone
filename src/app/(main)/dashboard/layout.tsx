091ad012b351d1c73b8d60258105624c4564433d
Micheal-Repo <michealfullstack@gmail.com>
2025-05-30 18:50:22 +0100

    updated

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,5 +1,9 @@
-import { SubscriptionModalProvider } from '@/lib/providers/subscription-modal-provider';
-import { getActiveProductsWithPrice } from '@/lib/supabase/queries';
+import {
+  SubscriptionModalProvider
+} from '@/lib/providers/subscription-modal-provider';
+import {
+  getActiveProductsWithPrice
+} from '@/lib/supabase/queries';
 import React from 'react';
 
 interface LayoutProps {
@@ -7,9 +11,20 @@ interface LayoutProps {
   params: any;
 }
 
-const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
-  const { data: products, error } = await getActiveProductsWithPrice();
-  if (error) throw new Error();
+const Layout: React.FC < LayoutProps > = async ({
+  children, params
+}) => {
+  const {
+    data: products,
+    error
+  } = await getActiveProductsWithPrice();
+
+
+  if (error) {
+    console.log("error in getActiveProductsWithPrice")
+    console.log(error)
+    throw new Error("error in getActiveProductsWithPrice");
+  }
   return (
     <main className="flex over-hidden h-screen">
       <SubscriptionModalProvider products={products}>
